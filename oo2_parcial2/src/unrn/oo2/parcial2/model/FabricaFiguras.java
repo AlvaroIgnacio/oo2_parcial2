afef905fdf26075c8e4d517c93e9478ed8275d15
AlvaroIgnacio <abayon@gmail.com>
2020-06-17 13:25:08 -0300

    creo una unica instancia de FiguraNula

--- oo2_parcial2/src/unrn/oo2/parcial2/model/FabricaFiguras.java
+++ oo2_parcial2/src/unrn/oo2/parcial2/model/FabricaFiguras.java
@@ -13,11 +13,13 @@ public class FabricaFiguras {
 	private HashMap<TipoFigura, Figura> templateFigura = new HashMap<>();
 
 	public FabricaFiguras() {
+		// FiguraNula no tiene estado -> puedo asignar una unica instancia
+		Figura nula = new FiguraNula();
 		this.templateFigura.put(TipoFigura.RECTANGULO, new Rectangulo(2,3));
 		this.templateFigura.put(TipoFigura.TRIANGULO, new Triangulo(3, 3, 3));
 		
 		// Uso del patron Objeto Nulo
-		this.templateFigura.put(TipoFigura.NULA, new FiguraNula());
+		this.templateFigura.put(TipoFigura.NULA, nula);
 	}
 	
 	public Figura crear(TipoFigura figura) {
